e1c8f255454742c444881168f4637770350ef2e5
Allain Dollete <allain.dollete@learnosity.com>
2021-06-15 11:28:15 -0400

    Move SDK logic to services directly

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LrnSDKService } from '../../services/lrn-sdk.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,13 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private lrnSDKService: LrnSDKService) { }
 
   ngOnInit(): void {
   }
 
   initAuthorAPI() {
     console.log("Initializing Author API");
+    let service = "author";
+    let securityPacket = {
+      "consumer_key": "yis0TYCu7U9V4o7M",
+      "domain":       "localhost",
+      "user_id":      "$ANONYMIZED_USER_ID"
+    };
+    let secret = "74c5fd430cf1242a527f6223aebd42d30464be22";
+    let requestPacket = {
+      "mode": "item_list",
+      "user": {
+        "id": "allain.dollete@learnosity.com"
+      }
+    };
+    let action = "";
+
+    let signedRequest = this.lrnSDKService.init(service, securityPacket, secret, requestPacket, action);
+    console.log(signedRequest);
+    
     
   }
 
--- src/app/services/lrn-sdk.service.ts
+++ src/app/services/lrn-sdk.service.ts
@@ -1,16 +1,158 @@
 import { Injectable } from '@angular/core';
-// import { Observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 // import { HttpClient, HttpHeaders } from '@angular/common/http';
-import * as LearnosityModule from 'learnosity-sdk-nodejs';
+// import * as LearnosityModule from 'learnosity-sdk-nodejs';
+import * as sha256 from 'crypto-js/sha256';
+import * as _ from 'underscore';
+import * as moment from 'moment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class LrnSDKService {
-
+  
   constructor() { }
 
-  testSDK() {
-    // let learnositySdk = new LearnosityModule();
+  hashSignatureArray(signatureArray) {
+    return sha256(signatureArray.join('_')).toString();
   }
+
+  convertRequestPacketToObject(requestPacket) {
+    if (_.isString(requestPacket)) {
+      return JSON.parse(requestPacket);
+    } else {
+      return requestPacket;
+    }
+  }
+
+  insertSecurityInformationToAssessObject(requestPacket, securityPacket, secret) {
+    if (requestPacket.questionsApiActivity) {
+      var questionsApi = requestPacket.questionsApiActivity;
+      var domain = 'assess.learnosity.com';
+
+      if (securityPacket.domain) {
+        domain = securityPacket.domain;
+      } else if (questionsApi.domain) {
+        domain = questionsApi.domain;
+      }
+
+      requestPacket.questionsApiActivity.consumer_key = securityPacket.consumer_key;
+      requestPacket.questionsApiActivity.timestamp = securityPacket.timestamp;
+      requestPacket.questionsApiActivity.user_id = securityPacket.user_id;
+      requestPacket.questionsApiActivity.signature = this.hashSignatureArray([
+        securityPacket.consumer_key,
+        domain,
+        securityPacket.timestamp,
+        securityPacket.user_id,
+        secret
+      ]);
+    }
+  }
+
+  generateSignature(
+    service,
+    securityPacket,
+    secret,
+    requestString,
+    action
+  ) {
+    let signatureArray = [
+      securityPacket.consumer_key,
+      securityPacket.domain,
+      securityPacket.timestamp
+    ];
+
+    if (securityPacket.user_id) {
+      signatureArray.push(securityPacket.user_id);
+    }
+    signatureArray.push(secret);
+
+    // Add the requestPacket if necessary
+    var signRequestData = !(service === 'assess' || service === 'questions');
+
+    if (signRequestData && requestString && requestString.length > 0) {
+      signatureArray.push(requestString);
+    }
+
+    // Add the action if necessary
+    if (action && action.length > 0) {
+      signatureArray.push(action);
+    }
+
+    return this.hashSignatureArray(signatureArray);
+  }
+
+  init = function (
+    service,
+    securityPacket,
+    secret,
+    requestPacket,
+    action
+  ) {
+    // requestPacket can be passed in as an object or as an already encoded
+    // string.
+    var requestObject = this.convertRequestPacketToObject(requestPacket);
+
+    // if (telemetryEnabled) {
+    //     addTelemetryData(requestObject);
+    // }
+
+    // Automatically timestamp the security packet
+    if (!securityPacket.timestamp) {
+      securityPacket.timestamp = moment().utc().format('YYYYMMDD-HHmm');
+    }
+
+    if (service === 'assess') {
+      this.insertSecurityInformationToAssessObject(requestObject, securityPacket, secret);
+    }
+
+    // Automatically populate the user_id of the security packet.
+    if (_.contains(['author', 'items', 'reports'], service)) {
+      // The Events API requires a user_id, so we make sure it's a part
+      // of the security packet as we share the signature in some cases
+      if (!securityPacket.user_id && requestObject && requestObject.user_id) {
+        securityPacket.user_id = requestObject.user_id;
+      }
+    }
+
+    var requestString = JSON.stringify(requestObject);
+
+    // Generate the signature based on the arguments provided
+    securityPacket.signature = this.generateSignature(
+      service,
+      securityPacket,
+      secret,
+      requestString,
+      action
+    );
+
+    var output;
+
+    if (service === 'data') {
+        output = {
+          'security': JSON.stringify(securityPacket),
+          'request': requestString,
+          'action': action
+        };
+    } else if (service === 'questions') {
+      // Questions API Requests don't need `domain`
+      delete securityPacket.domain;
+
+      output = _.extend(securityPacket, requestObject);
+    } else if (service === 'assess') {
+      output = requestObject;
+    } else {
+      output = {
+        'security': securityPacket,
+        'request': _.isString(requestPacket) ? requestString : requestObject
+      };
+    }
+    // console.log(output);
+    
+    return output;
+  };
+
+  
+
   
 }
